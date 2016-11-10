e8e0ae8a5ad326a68bb34d80bd95112a02ae4523
Matthias Lambrecht <sirfelius-r15@Matthias-no-MacBook-Pro.local>
2016-11-10 13:49:15 +0900

    Continued WebAudio implementation

--- js/web_audio.js
+++ js/web_audio.js
@@ -5,14 +5,14 @@ document.addEventListener('DOMContentLoaded', function() {
 
 	/* 
 	 * Click event listener for this specific id
-	 */
 	document.getElementById('wa-01-play').addEventListener('click', function() {
 		alert('Clicked wa-01-play');
 	});
+	 */
+
 
 	/*
 	 * Make all buttons output their name to console when clicked.
-	 */
 	var buttons = document.getElementsByClassName('btn');
 
 	var myFunction = function() {
@@ -24,16 +24,48 @@ document.addEventListener('DOMContentLoaded', function() {
 		// buttons[i].addEventListener('click', myFunction, false);
 		buttons[i].addEventListener('click', myFunction, false);
 	}
+	 */
+
+
+	/*
+	  Simple Audio output using WebAudio API.
+	  See http://techblog.stevej.name/2016/04/08/making-music-with-the-webaudio-api-part-1/
+	*/
+	(function() {
+		// Set up variables
+		var playToneEl 		= document.getElementById('wa-01-play'),
+		    stopToneEl		= document.getElementById('wa-01-stop'),
+		    AudioContext	= (window.AudioContext || window.webkitAudioContext),
+		    audioCtx 		= new AudioContext(),
+		    oscillator;
+
+		// Add EventListeners to buttons in DOM
+		playToneEl.addEventListener('click', playTone, false);
+		stopToneEl.addEventListener('click', stopTone, false);
+
+		// Functions to start and stop audio
+		function playTone() {
+			// Set frequency and connect oscillator to output.
+			oscillator = audioCtx.createOscillator();
+			oscillator.frequency.value = 440; 	// 440 Hz = Middle A Natural
+			oscillator.connect(audioCtx.destination);
+
+			oscillator.start();
+			stopToneEl.disabled = false;
+			stopToneEl.focus();
+			playToneEl.disabled = true;
+		}
+
+		function stopTone() {
+			oscillator.stop();
+			stopToneEl.disabled = true;
+			playToneEl.disabled = false;
+		}
+	})();
+
+
 });
 
 
-const alertMessage = function() {
-	// Displays a simple alert.
-	alert('Triggered.');
-};
 
 
-/*
-  Simple Audio output using WebAudio API.
-  See: http://techblog.stevej.name/2016/04/08/making-music-with-the-webaudio-api-part-1/
-*/
